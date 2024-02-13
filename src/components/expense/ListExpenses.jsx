bab76492c68c6010744a6f2e221f95d9904e9cb2
samirvithlani <samir.vithlani83955@gmail.com>
2024-02-13 20:41:31 +0530

    chart grid

--- src/components/expense/ListExpenses.jsx
+++ src/components/expense/ListExpenses.jsx
@@ -1,9 +1,33 @@
 import { createTheme, Grid, ThemeProvider } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const ListExpenses = () => {
+  const data = {
+    labels: ['Debit', 'Cash', 'Credit'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
   const columns = [
     { field: "title", headerName: "Title", width: 150 },
     { field: "amount", headerName: "Amount", width: 150 },
@@ -29,7 +53,11 @@ export const ListExpenses = () => {
           columns={columns}
           rows={expenses}
           getRowId={(row) => row._id}
+          slots= {{toolbar:GridToolbar}}
         ></DataGrid>
+        <Grid item xs={6} sx = {{height:400,width:400}}>
+        <Pie data={data} />
+        </Grid>
       </Grid>
     </ThemeProvider>
   );
